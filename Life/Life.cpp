7d82a664b0c78cb586efae1c75a3668a3acd282a
GSonderling <jirka.win@gmail.com>
2015-05-30 15:07:00 +0200

    First commit

---
+++ Life/Life.cpp
@@ -0,0 +1,123 @@
+// Life.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <math.h>
+#include <stdlib.h>
+#include <time.h> 
+#include <iostream>
+#include <Windows.h>
+
+
+int oldfield[500][500];
+int newfield[500][500];
+double decrandom(){
+	double ran; 
+	ran = (float) rand()/RAND_MAX;
+	return ran;
+}
+int binrandom(){
+	int ran;
+	ran = rand();
+	if (ran % 2 == 0){
+		ran = 1;
+	}
+	else {
+		ran = 0;
+	}
+	return ran;
+}
+int fieldStp(){
+	int x, y;
+	int field[500][500];
+	for (x = 0; x <= 499; x = x + 1){
+		for (y = 0; y <= 499; y = y + 1){
+			field[x][y] = binrandom();
+			oldfield[x][y] = field[x][y];
+		}
+	}
+	return 0;
+}
+int fieldref(){
+	int sum;
+	for (int x = 0; x <= 499; x = x + 1){
+		for (int y = 0; y <= 499; y = y + 1){
+			sum = oldfield[x + 1][y] + oldfield[x - 1][y] + oldfield[x][y + 1] + oldfield[x][y - 1] + oldfield[x + 1][y + 1] + oldfield[x - 1][y + 1] + oldfield[x - 1][y - 1] + oldfield[x + 1][y - 1];
+			if (sum >3 || sum < 2){
+				newfield[x][y] = 0;
+			}
+			if (sum == 3){
+				newfield[x][y] = 1;
+
+
+			}
+			if ((sum == 2 || sum == 3) && oldfield[x][y] == 1){
+				newfield[x][y] = oldfield[x][y];
+			}
+		}
+	}
+	for (int x = 0; x <= 499; x = x + 1){
+		for (int y = 0; y <= 499; y = y + 1){
+			oldfield[x][y] = newfield[x][y];
+		}
+	}
+	return 0;
+}
+int main(int argc, char **argv){
+	bool quit = false;
+	fieldStp();
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = NULL;
+	window = SDL_CreateWindow("Life", 100, 100, 1000, 1000, SDL_WINDOW_SHOWN);
+	if (window == NULL){
+		printf("ERROR Window creation failed");
+		return 0;
+	}
+	SDL_Renderer* renderer = NULL;
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Event* mainEvent = new SDL_Event();
+	SDL_Texture* replicator = NULL;
+	replicator = IMG_LoadTexture(renderer, "replicator.jpg");
+	SDL_Texture* dead = NULL;
+	dead = IMG_LoadTexture(renderer, "dead.jpg");
+	SDL_Rect replicator_rect;
+	SDL_Rect dead_rect;
+	while (quit == false && mainEvent->type != SDL_QUIT){
+		int x, y;
+
+		SDL_PollEvent(mainEvent);
+		SDL_RenderClear(renderer);
+		fieldref();
+
+
+			for (x = 0; x <= 499; x = x + 1){
+			for (y = 0; y <= 499; y = y + 1){
+				if (oldfield[x][y] == 1){
+					replicator_rect.x = 2 * x;
+					replicator_rect.y = 2 * y;
+					replicator_rect.w = 2;
+					replicator_rect.h = 2;
+	                SDL_RenderCopy(renderer, replicator, NULL, &replicator_rect);
+				}
+				if (oldfield[x][y] == 0){
+					dead_rect.x = 2 * x;
+					dead_rect.y = 2 * y;
+					dead_rect.w = 2;
+					dead_rect.h = 2;
+				    SDL_RenderCopy(renderer, dead, NULL, &dead_rect);
+
+				}
+			
+
+			}
+		}
+
+		SDL_RenderPresent(renderer);
+		Sleep(100);
+
+	}
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	delete mainEvent;
+	return 0;
+}
